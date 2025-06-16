f781aca8a0c21cef9e90aec2f059916d554e171b
akashgupta213 <akash.chandra9867@gmail.com>
2025-06-17 00:28:53 +0530

    Update Register.jsx

--- e_commerce/src/components/Auth/Register.jsx
+++ e_commerce/src/components/Auth/Register.jsx
@@ -14,7 +14,7 @@ const Register = () => {
     console.log("🔴 Register button clicked");
 
     try {
-      const res = await axios.post("http://localhost:5000/api/auth/register", {
+      const res = await axios.post("https://gshop-backend.onrender.com", {
         username,
         email,
         password,
