e5db39e326e5b934b3209c7f345af72dbcc715bb
akashgupta213 <akash.chandra9867@gmail.com>
2025-06-17 00:27:52 +0530

    Update Login.js

--- e_commerce/src/components/Auth/Login.js
+++ e_commerce/src/components/Auth/Login.js
@@ -15,7 +15,7 @@ const handleSubmit = async (e) => {
   e.preventDefault();
 
   try {
-    const res = await fetch("http://localhost:5000/api/auth/login", {
+    const res = await fetch("https://gshop-backend.onrender.com", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
