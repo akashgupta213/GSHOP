a1888a5a82d199cc7926ecc479cca4f8ed68ff4c
akashgupta213 <akash.chandra9867@gmail.com>
2025-06-17 20:06:29 +0530

    Update Login.jsx

--- e_commerce/src/components/Auth/Login.jsx
+++ e_commerce/src/components/Auth/Login.jsx
@@ -10,7 +10,7 @@ const Login = () => {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await axios.post("https://g-shop-backend.onrender.com", {
+      const res = await axios.post("/api/users/login", {
         email,
         password,
       });
